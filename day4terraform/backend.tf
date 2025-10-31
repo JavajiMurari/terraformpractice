b0376f3dea4cbfffd9361738afb2f75cf05125b0
MurariMittuu <murarimudhiraj66@gmail.com>
2025-10-31 11:55:53 +0530

    day1

--- day4terraform/backend.tf
+++ day4terraform/backend.tf
@@ -2,9 +2,9 @@ terraform {
   backend "s3" {
     bucket = "day4terraformbucket"
     key    = "terraform.tfstate"
-    #use_lockfile = true # to use s3 native locking
+    use_lockfile = true # to use s3 native locking 1.13.4 version above
     region = "us-east-1"
-    dynamodb_table = "veera" # any version we can use dynamodb locking 
+    dynamodb_table = "murari" # any version we can use dynamodb locking 
     encrypt = true
   }
 }
