82c6933ab4d35a0c1eb0028f7564181ccf37b694
MurariMittuu <murarimudhiraj66@gmail.com>
2025-11-04 21:12:41 +0530

    updated

---
+++ day-6-import/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "name" {
+  ami = "ami-0bdd88bd06d16ba03"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "tf-import"
+  }
+
+}
+
+
+#example command terraform import aws_instance.name i-0f805ae729b101f2f
--- day-7-values/main.tf
+++
@@ -1,5 +0,0 @@
-module "vpc" {
-  source              = "../day-7-modules"   # Path to your VPC module folder
-  vpc_name            = "dev-vpc"            # Name for your VPC
-  cidr_block          = "10.0.0.0/16"        # CIDR range for the VPC
-}
